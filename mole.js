2ac74f76ff1411e863cb4c2d5c3683980347d8ce
itsmepravin <singh.pravin19@yahoo.com>
2024-02-03 14:13:23 +0545

    added pipes in the board

--- mole.js
+++ mole.js
@@ -1,1 +1,12 @@
-console.log("Hello from Whack a Mole!");
+window.onload = function () {
+  setGame();
+};
+
+function setGame() {
+  // Setup the grid for the game board in html
+  for (let i = 0; i < 9; i++) {
+    let tile = document.createElement("div");
+    tile.id = i.toString();
+    document.getElementById("board").appendChild(tile);
+  }
+}
